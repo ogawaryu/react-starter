6bd9347b25e9fed4195f7079736072b899a59ad5
Ryu <ogawaryuichi@gmail.com>
2017-04-02 00:42:45 -0300

    convert tabs to space

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -5,18 +5,18 @@ const DEVELOPMENT = process.env.NODE_ENV === 'development'
 const PRODUCTION = process.env.NODE_ENV === 'production'
 
 module.exports = {
-	devtool: 'source-map',
-	entry: './src/app.js',
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			loaders: ['babel-loader'],
-			exclude: /node_modules/
-		}]
-	},
-	output: {
-		path: path.join(__dirname, 'dist'),
-		publicPath: PRODUCTION ? '/' : '/dist/',
-		filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
-	}
+  devtool: 'source-map',
+  entry: './src/app.js',
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['babel-loader'],
+      exclude: /node_modules/
+    }]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: PRODUCTION ? '/' : '/dist/',
+    filename: PRODUCTION ? 'bundle.[hash:12].min.js' : 'bundle.js'
+  }
 };
